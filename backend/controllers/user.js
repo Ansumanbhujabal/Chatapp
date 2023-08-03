fc8f468ab433929ec7d23b092e6ec0a3a0ed6d44
Ansumanbhujabal <106860608+Ansumanbhujabal@users.noreply.github.com>
2023-08-03 16:45:13 +0530

    test success

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,21 +2,21 @@ const user = require("../models/user");
 
 exports.register = async (req, res) => {
   try {
-    const { name, email, apssword } = req.body;
-    let user = user.findOne({ email });
-    if (user)
+    const { name, email, password } = req.body;
+    let existingUser = await user.findOne({ email });
+    if (existingUser)
       return res.status(400).json({
         success: false,
         message: "user already exist",
       });
-    user = await user.create({
+    const newUser = await user.create({
       name,
       email,
       password,
       avatar: { public_id: "string", url: "string" },
     });
 
-    res.status(201).json({ success: true, user });
+    res.status(201).json({ success: true, user: newUser });
   } catch (error) {
     res.status(500).json({
       success: false,
