6942c89e94a827e767fc9a93442bdcdafa7299cc
Ansumanbhujabal <106860608+Ansumanbhujabal@users.noreply.github.com>
2023-08-18 19:00:43 +0530

    Reducer Minor update

--- frontend/src/Reducers/user.js
+++ frontend/src/Reducers/user.js
@@ -1,6 +1,6 @@
 import { createReducer } from "@reduxjs/toolkit";
 
-const initialState = {};
+const initialState = { isAuthenticated: false };
 
 export const userReducer = createReducer(initialState, {
   LoginRequest: (state) => {
@@ -9,10 +9,12 @@ export const userReducer = createReducer(initialState, {
   LoginSuccess: (state, action) => {
     state.loading = false;
     state.user = action.payload;
+    state.isAuthenticated = true;
   },
   LoginFailure: (state, action) => {
     state.loading = false;
     state.error = action.payload;
+    state.isAuthenticated = false;
   },
 
   RegisterRequest: (state) => {
@@ -21,21 +23,39 @@ export const userReducer = createReducer(initialState, {
   RegisterSuccess: (state, action) => {
     state.loading = false;
     state.user = action.payload;
+    state.isAuthenticated = true;
   },
   RegisterFailure: (state, action) => {
     state.loading = false;
     state.error = action.payload;
+    state.isAuthenticated = false;
   },
 
-  LoadUserRequset: (state) => {
+  LoadUserRequest: (state) => {
     state.loading = true;
   },
   LoadUserSuccess: (state, action) => {
     state.loading = false;
     state.user = action.payload;
+    state.isAuthenticated = true;
   },
   LoadUserFailure: (state, action) => {
     state.loading = false;
     state.error = action.payload;
+    state.isAuthenticated = false;
   },
+
+  // LogoutUserRequest: (state) => {
+  //   state.loading = true;
+  // },
+  // LogoutUserSuccess: (state) => {
+  //   state.loading = false;
+  //   state.user = null;
+  //   state.isAuthenticated = false;
+  // },
+  // LogoutUserFailure: (state, action) => {
+  //   state.loading = false;
+  //   state.error = action.payload;
+  //   state.isAuthenticated = true;
+  // },
 });
