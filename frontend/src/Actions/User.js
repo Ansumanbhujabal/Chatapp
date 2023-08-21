ddba0a32c4cd6d0ca06a11f1d4e15e6149d9c7a3
Ansumanbhujabal <106860608+Ansumanbhujabal@users.noreply.github.com>
2023-08-21 22:22:41 +0530

    Get following posts add

--- frontend/src/Actions/User.js
+++ frontend/src/Actions/User.js
@@ -41,3 +41,22 @@ export const loadUser = () => async (dispatch) => {
     });
   }
 };
+
+export const getFollowingPosts = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: "postOfFollowingRequest",
+    });
+    const { data } = await axios.get("/api/v1/posts");
+
+    console.log(data);
+    dispatch({
+      type: "postOfFollowingSuccess",
+    });
+  } catch (error) {
+    dispatch({
+      type: "postOfFollowingFailure",
+      payload: error.response.data.message,
+    });
+  }
+};
