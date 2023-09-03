0da488d35c8697b9e9022390596197fd0b4f8771
Ansumanbhujabal <106860608+Ansumanbhujabal@users.noreply.github.com>
2023-09-03 16:39:49 +0530

    Update caption action add

--- frontend/src/Actions/post.js
+++ frontend/src/Actions/post.js
@@ -96,3 +96,24 @@ export const createNewPost = (caption, image) => async (dispatch) => {
     });
   }
 };
+
+export const updatePost = (caption, id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: "updateCaptionRequest",
+    });
+    const { data } = await axios.put(`/api/v1/post/${id}`, {
+      caption,
+    });
+
+    dispatch({
+      type: "updateCaptionSuccess",
+      payload: data.message,
+    });
+  } catch (error) {
+    dispatch({
+      type: "updateCaptionFailure",
+      payload: error.response.data.message,
+    });
+  }
+};
