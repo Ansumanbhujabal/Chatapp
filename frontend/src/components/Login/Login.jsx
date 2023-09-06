0ec994add65b47b65b3b07949e02d739abbecd37
Ansumanbhujabal <106860608+Ansumanbhujabal@users.noreply.github.com>
2023-09-06 15:13:16 +0530

    Minor Login update  add

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,20 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Login.css";
 import { Typography, Button } from "@mui/material";
 import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loginUser } from "../../Actions/User";
+import { toast } from "react-toastify";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const dispatch = useDispatch();
+  const { error } = useSelector((state) => state.user);
+  const { message } = useSelector((state) => state.like);
 
   const loginHandler = (e) => {
     e.preventDefault();
 
     dispatch(loginUser(email, password));
   };
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+      dispatch({ type: "clearErrors" });
+    }
+    if (message) {
+      toast.success(message);
+      dispatch({ type: "clearMessage" });
+    }
+  }, [toast, error, dispatch, message]);
 
   return (
     <div className="login">
