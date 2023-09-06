e39fcee8aa2b7b7ad960f710ab4017509351295b
Ansumanbhujabal <106860608+Ansumanbhujabal@users.noreply.github.com>
2023-09-06 15:12:10 +0530

    Update Password Action add

--- frontend/src/Actions/User.js
+++ frontend/src/Actions/User.js
@@ -69,6 +69,30 @@ export const updateProfile = (name, email, avatar) => async (dispatch) => {
   }
 };
 
+export const updatePassword =
+  (oldPassword, newPassword) => async (dispatch) => {
+    try {
+      dispatch({ type: "updatePasswordRequest" });
+
+      const { data } = await axios.put(
+        "/api/v1/update/password",
+        { oldPassword, newPassword },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      dispatch({ type: "updatePasswordSuccess", payload: data.message });
+    } catch (error) {
+      dispatch({
+        type: "updatePasswordFailure",
+        payload: error.response.data.message,
+      });
+    }
+  };
+
 export const loadUser = () => async (dispatch) => {
   try {
     dispatch({
